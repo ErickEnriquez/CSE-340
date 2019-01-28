20f901cd71a9a81c8d45e86d91cd984b8df66cef
Erick <lapiedad2010@gmail.com>
2019-01-28 14:11:45 -0700

    it seems to work for the t1_ss test case

--- parser.cc
+++ parser.cc
@@ -46,208 +46,209 @@ Token Parser::peek()
 // Parsing//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //parses the begining of the input
-void Parser::parse_input(){
+void Parser::parse_input() {
 	parse_program();
 	parse_inputs();
-		cout<<"parse successful\n";
-		return;
+	cout << "parse successful\n";
+	return;
 }
 
-void Parser::parse_program(){
-	Token t;
-	Parser p;
-	t = p.peek();
-	if(t.token_type == MAIN){
+void Parser::parse_program() {
+	Token t = lexer.GetToken();
+	if (t.token_type == MAIN) {
+		lexer.UngetToken(t);
 		parse_main();
 	}
-	else if(t.token_type == PROC){
+	else if (t.token_type == PROC) {
 		parse_proc_decl_section();
 		parse_main();
 	}
-	else{
+	else {
 		syntax_error();
 	}
 	return;
 }
 
-void Parser::parse_proc_decl_section(){
+void Parser::parse_proc_decl_section() {
 	Token t = lexer.GetToken();// if main we are done with this section
-	if(t.token_type == MAIN){
+	if (t.token_type == MAIN) {
 		lexer.UngetToken(t);
 		return;
 	}
-	else if(t.token_type == PROC){
+	else if (t.token_type == PROC) {
 		lexer.UngetToken(t);
 		parse_proc_decl();
 		parse_proc_decl_section();
 	}
 	else
-		syntax_error();	
+		syntax_error();
 }
 
-void Parser::parse_proc_decl(){
+void Parser::parse_proc_decl() {
 	Token t1 = lexer.GetToken();//PROC
-	if(t1.token_type != PROC){
+	if (t1.token_type != PROC) {
 		syntax_error();
 	}
 }
 //parses main in input
-void Parser::parse_main(){
+void Parser::parse_main() {
 	Token t = lexer.GetToken();//get the token but if we get the terminal that is expected you consume it
-	if(t.token_type == MAIN){
+	if (t.token_type == MAIN) {
 		parse_procedure_body();
 	}
-	else{
+	else {
 		syntax_error();
 	}
 	return;
 }
 
-void Parser::parse_procedure_body(){
+void Parser::parse_procedure_body() {
 	parse_statement_list();
 	return;
 }
-void Parser::parse_statement_list(){
+void Parser::parse_statement_list() {
 	Token t = lexer.GetToken();//if we get a number we have finished parsing the statements and we are in the inputs
-	if( t.token_type != NUM){//while there are more statements to parse
+	if (t.token_type != NUM) {//while there are more statements to parse
+		lexer.UngetToken(t);
 		parse_statement();
 	}
 	return;
 }
 
-void Parser::parse_statement(){
+void Parser::parse_statement() {
 	Token t1 = lexer.GetToken();// INPUT || OUTPUT || ID || DO
-	if(t1.token_type == INPUT){
+	if (t1.token_type == INPUT) {
 		lexer.UngetToken(t1);
 		parse_input_statement();
 	}
-	else if(t1.token_type ==OUTPUT){
+	else if (t1.token_type == OUTPUT) {
 		lexer.UngetToken(t1);
 		parse_ouput_statement();
 	}
-	else if(t1.token_type == DO){
+	else if (t1.token_type == DO) {
 		lexer.UngetToken(t1);
 		parse_do_statement();
 	}
-	else if(t1.token_type == ID){
+	else if (t1.token_type == ID) {
 		Token t2 = lexer.GetToken();// EQUAL || SEMICOLON
-		if(t2.token_type == EQUAL){
+		if (t2.token_type == EQUAL) {
 			lexer.UngetToken(t2);
 			lexer.UngetToken(t1);
 			parse_assign_statement();
 		}
-		else if(t2.token_type == SEMICOLON){
+		else if (t2.token_type == SEMICOLON) {
 			lexer.UngetToken(t2);
 			lexer.UngetToken(t1);
 			parse_procedure_invocation();
 		}
-		else{
+		else {
 			syntax_error();
 		}
 	}
-	else{
+	else {
 		syntax_error();
 	}
 
-	
+
 }
 
-void Parser::parse_input_statement(){
+void Parser::parse_input_statement() {
 	Token t1 = lexer.GetToken();//INPUT
 	Token t2 = lexer.GetToken();//ID
-	if(t1.token_type != INPUT || t2.token_type != ID){
+	if (t1.token_type != INPUT || t2.token_type != ID) {
 		syntax_error();
 	}
 	parse_expr();
 	Token t3 = lexer.GetToken();//SEMICOLON
-	if(t3.token_type != SEMICOLON){
+	if (t3.token_type != SEMICOLON) {
 		syntax_error();
 	}
 	return;
 }
 
-void Parser::parse_ouput_statement(){
+void Parser::parse_ouput_statement() {
 	Token t1 = lexer.GetToken();//OUTPUT
 	Token t2 = lexer.GetToken();//ID
-	if(t1.token_type != OUTPUT || t2.token_type != ID){
+	if (t1.token_type != OUTPUT || t2.token_type != ID) {
 		syntax_error();
 	}
 	parse_expr();
 	Token t3 = lexer.GetToken();//SEMICOLON
-	if(t3.token_type != SEMICOLON){
+	if (t3.token_type != SEMICOLON) {
 		syntax_error();
 	}
-	return ;
+	return;
 }
 
-void Parser::parse_do_statement(){
+void Parser::parse_do_statement() {
 	Token t1 = lexer.GetToken();//DO
 	Token t2 = lexer.GetToken();//ID
-	if(t1.token_type != DO || t2.token_type != ID ){
+	if (t1.token_type != DO || t2.token_type != ID) {
 		syntax_error();
 	}
 	parse_procedure_invocation();
 	return;
 }
 
-void Parser::parse_procedure_invocation(){
+void Parser::parse_procedure_invocation() {
 	Token t1 = lexer.GetToken();//ID
-	Token t2  = lexer.GetToken();//SEMICOLON
-	if(t1.token_type != ID || t2.token_type != SEMICOLON){
+	Token t2 = lexer.GetToken();//SEMICOLON
+	if (t1.token_type != ID || t2.token_type != SEMICOLON) {
 		syntax_error();
 	}
 	return;
 }
 
-void Parser::parse_assign_statement(){
+void Parser::parse_assign_statement() {
 	Token t1 = lexer.GetToken();//ID
 	Token t2 = lexer.GetToken();//EQUAL
-	if(t1.token_type != ID || t2.token_type != EQUAL){
+	if (t1.token_type != ID || t2.token_type != EQUAL) {
 		syntax_error();
 	}
 	parse_expr();
 	Token t3 = lexer.GetToken();//SEMICOLON
-	if(t3.token_type != SEMICOLON){
+	if (t3.token_type != SEMICOLON) {
 		syntax_error();
 	}
 	return;
 }
 
-void Parser::parse_operator(){
+void Parser::parse_operator() {
 	Token t = lexer.GetToken();
-	if(t.token_type != PLUS || t.token_type != MINUS || t.token_type != DIV || t.token_type != MULT)
+	if (t.token_type != PLUS || t.token_type != MINUS || t.token_type != DIV || t.token_type != MULT)
 		syntax_error();
-	return ;
+	return;
 }
 
-void Parser::parse_procedure_name(){
+void Parser::parse_procedure_name() {
 	Token t = lexer.GetToken();
-	if(t.token_type != ID || t.token_type != NUM)
+	if (t.token_type != ID || t.token_type != NUM)
 		syntax_error();
-	return ;
+	return;
 }
 
-void Parser::parse_inputs(){
+void Parser::parse_inputs() {
 	Token t = lexer.GetToken();
-	if(t.token_type == NUM){
+	if (t.token_type == NUM) {
 		//do stuff here
 		parse_inputs();
 	}
-	else if(t.token_type == END_OF_FILE){
+	else if (t.token_type == END_OF_FILE) {
 		//program is done parsing input
 	}
-	else{
+	else {
 		syntax_error();
 	}
 	return;
 }
 
-void Parser::parse_expr(){
+void Parser::parse_expr() {
 	Token t = lexer.GetToken();//If we get a semicolon then we are done parsing expression
-	if(t.token_type == SEMICOLON){
+	if (t.token_type == SEMICOLON) {
+		lexer.UngetToken(t);
 		return;
 	}
-	else{
+	else {
 		parse_primary();
 		parse_operator();
 		parse_primary();
@@ -255,15 +256,15 @@ void Parser::parse_expr(){
 }
 
 
-void Parser::parse_primary(){
-	Token t= lexer.GetToken(); //ID || NUM
-	if(t.token_type == ID){
+void Parser::parse_primary() {
+	Token t = lexer.GetToken(); //ID || NUM
+	if (t.token_type == ID) {
 
 	}
-	else if(t.token_type == NUM ){
+	else if (t.token_type == NUM) {
 
 	}
-	else{
+	else {
 		syntax_error();
 	}
 	return;
@@ -274,6 +275,7 @@ int main()
 {
 	LexicalAnalyzer lexer;
 	Token token;
-	
-	
+	Parser p;//make a parser object to parse the input
+	p.parse_input();
+
 }
